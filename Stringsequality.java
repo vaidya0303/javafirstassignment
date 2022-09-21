1e8875cff001e9452836cd5a8fb84db5e4611e2e
vaidya0303 <vaidya0304@gmail.com>
2022-09-21 18:18:04 +0530

    equality check of 2 strings

---
+++ Stringsequality.java
@@ -0,0 +1,18 @@
+public class Stringsequality {
+    public static void main(String[] args) {
+        String A = "Saurabh";
+        String B = "Vaidya";
+
+        if (A==B) {
+            System.out.println("strings are equal");
+        }
+
+        else  {
+            System.out.println("strings are not equal");
+            }
+
+         }
+
+    }
+            
+    
